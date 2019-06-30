9f4265a0c8a63ee9434a46dbacc6ae728c36376f
sunpengfei <1486859321@qq.com>
2019-06-30 20:53:24 +0800

    第一次更改了java代码

--- src/main/java/itcast/TestGit.java
+++ src/main/java/itcast/TestGit.java
@@ -2,6 +2,9 @@ package itcast;
 
 public class TestGit {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        int i=10;
+        System.out.println(i);
     }
 }
